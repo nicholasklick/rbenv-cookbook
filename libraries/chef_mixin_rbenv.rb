17a266cea74ef28d154abb50099f15e8e7ac1c68
nicholasklick <nicholas.klick@gmail.com>
2017-07-03 13:24:02 -0400

    Fix some deprecations part 1

--- libraries/chef_mixin_rbenv.rb
+++ libraries/chef_mixin_rbenv.rb
@@ -122,25 +122,18 @@ class Chef
 
         time = Time.now.to_i
 
-        ruby_block "set HOME for #{username} at #{time}" do
-          block do
-            ENV['OLD_HOME'] = ENV['HOME']
-            ENV['HOME'] = begin
-              require 'etc'
-              Etc.getpwnam(username).dir
-            rescue ArgumentError # user not found
-              "/home/#{username}"
-            end
+        Chef::Log.info("set HOME for #{username} at #{time}")
+        ENV['OLD_HOME'] = ENV['HOME']
+        ENV['HOME'] = \
+          begin
+            require 'etc'
+            Etc.getpwnam(username).dir
+          rescue ArgumentError # user not found
+            "/home/#{username}"
           end
-        end
 
         yield
 
-        ruby_block "unset HOME for #{username} #{time}" do
-          block do
-            ENV['HOME'] = ENV['OLD_HOME']
-          end
-        end
       end
     end
   end
